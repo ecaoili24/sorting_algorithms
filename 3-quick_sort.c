a23ed3dd760a42dbd8bb2e325e751e0325248747
ecaoili24 <erika.caoili@gmail.com>
2020-02-02 21:44:18 +0000

    modified the quick sort file and added the swap file, deleted the swap file

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,7 +10,7 @@
 
 void quick_sort(int *array, size_t size)
 {
-	qwiksort(array, 0, (int) size - 1, size); 
+	qwiksort(array, 0, (int) size - 1, size);
 }
 
 
@@ -40,10 +40,23 @@ int partition(int *array, int lo, int hi, size_t size)
 		{
 			/* increment i then swap*/
 			i += 1;
-			swap(&array[i], &array[j]);	
+			swap(&array[i], &array[j]);
 		}
-	}	
+	}
 	swap(&array[i + 1], &array[hi]);
 	print_array(array, size);
 	return (i + 1);
+}
+/**
+ * swap - swaps the 2 elements of an array
+ * @idx1: index1
+ * @idx2: index2
+ *
+ * Return: None
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
