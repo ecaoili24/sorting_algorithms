d9e0416e29215f644bd11ef3130d65459d3c8998
kevapostol <apostolkevin@gmail.com>
2020-02-02 17:06:04 +0000

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: None
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, p1, p2;
+	size_t size2 = size;
+
+	for (i = 0; i <= size - 1; i += 1)
+	{
+		for (j = 0; j < size2 - i - 1; j += 1)
+		{
+			p1 = array[j];
+			p2 = array[j + 1];
+
+			if (p1 > p2)
+			{
+				int temp = p1;
+
+				p1 = p2;
+				p2 = temp;
+
+				array[j] = p1;
+				array[j + 1] = p2;
+				print_array(array, size);
+			}
+		}
+	}
+}
