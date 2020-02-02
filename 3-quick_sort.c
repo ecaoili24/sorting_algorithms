b3987739ebc320e2fdc4162595758d2b2d30e9c5
kevapostol <apostolkevin@gmail.com>
2020-02-02 21:34:06 +0000

    Quick sort and swap

---
+++ 3-quick_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: None
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	qwiksort(array, 0, (int) size - 1, size); 
+}
+
+
+void qwiksort(int *array, int lo, int hi, size_t size)
+{
+	if (lo < hi)
+	{
+		/*pivot*/
+		int p = partition(array, lo, hi, size);
+		/*left partition*/
+		qwiksort(array, lo, p - 1, size);
+
+		/*right parititon*/
+		qwiksort(array, p + 1, hi, size);
+	}
+}
+
+
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int pivot = array[hi];
+	int i = (lo - 1), j;
+
+	for (j = lo; j <= hi - 1; j += 1)
+	{
+		if (array[j] < (int) pivot)
+		{
+			/* increment i then swap*/
+			i += 1;
+			swap(&array[i], &array[j]);	
+		}
+	}	
+	swap(&array[i + 1], &array[hi]);
+	print_array(array, size);
+	return (i + 1);
+}
