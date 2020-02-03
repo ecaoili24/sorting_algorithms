7a1286df658ab24833833e28a2a01c9a29b3dc34
kevapostol <apostolkevin@gmail.com>
2020-02-03 00:12:36 +0000

    Modified code

--- sort.h
+++ sort.h
@@ -25,7 +25,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void swap(int *a, int *b);
-void swap_arr(int *array, int idx1, int idx2);
+void swap_arr(int *a, int *b);
 void quick_sort(int *array, size_t size);
 void qwiksort(int *array, int lo, int hi, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
