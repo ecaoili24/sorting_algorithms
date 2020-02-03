812786061ff6b1eb27eb66b075e2388106c98bcf
ecaoili24 <erika.caoili@gmail.com>
2020-02-03 20:54:37 +0000

    added the prototype for the function

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algo
+ * @n: the data
+ * @list: the  list to sort
+ *
+ * Return: None
+ */
+void insertion_sort_list(listint_t **list)
+{
