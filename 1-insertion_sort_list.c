5a0f6e7cd5c7639422b055921b9a9472cd53f16a
ecaoili24 <erika.caoili@gmail.com>
2020-02-03 21:52:26 +0000

    added the insertion sort program

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,11 +2,55 @@
 
 /**
  * insertion_sort_list - a function that sorts a doubly linked list
- * of integers in ascending order using the Insertion sort algo
- * @n: the data
+ * of integers in ascending order using the Insertion sort algorithm
  * @list: the  list to sort
  *
  * Return: None
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *temp, *current, *t_list;
+	int elem;
+
+	for (temp = (*list)->next; temp != NULL; temp = temp->next)
+	{
+		elem = temp->n;
+
+		for (current = temp->prev; current != NULL; current = current->prev)
+		{
+			if (elem < current->n)
+			{
+				swap_nodes(current, temp);
+				continue;
+			}
+			break;
+		}
+
+		for (t_list = *list; t_list != NULL; t_list = t_list->prev)
+		{
+			*list = t_list;
+			print_list(*list);
+		}
+	}
+}
+
+/**
+ * swap_nodes - function that swaps the nodes of a doubly linked list
+ * @nodeL: the left node
+ * @nodeR: the right node
+ *
+ * Return: None
+ */
+void swap_nodes(listint_t *nodeL, listint_t *nodeR)
+{
+	nodeL->next = nodeR->next;
+	nodeR->next = nodeL;
+	nodeR->prev = nodeL->prev;
+	nodeL->prev = nodeR;
+
+	if (nodeR->prev != NULL)
+		nodeR->prev->next = nodeR;
+
+	if (nodeL->next != NULL)
+		nodeL->next->prev = nodeL;
+}
