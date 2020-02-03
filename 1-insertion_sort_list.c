e1b80a4601f778231eeff5bde0ab83b19dd1a13e
ecaoili24 <erika.caoili@gmail.com>
2020-02-03 22:03:27 +0000

    debugged the code for insertion sort, removed outside for loop and created another nested for loop

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,7 +3,7 @@
 /**
  * insertion_sort_list - a function that sorts a doubly linked list
  * of integers in ascending order using the Insertion sort algorithm
- * @list: the  list to sort
+ * @list: the list to sort
  *
  * Return: None
  */
@@ -11,29 +11,22 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp, *current, *t_list;
 	int elem;
-
 	for (temp = (*list)->next; temp != NULL; temp = temp->next)
 	{
 		elem = temp->n;
-
 		for (current = temp->prev; current != NULL; current = current->prev)
 		{
 			if (elem < current->n)
 			{
 				swap_nodes(current, temp);
+				for (t_list = *list; t_list != NULL; t_list = t_list->prev)
+					*list = t_list;
+				print_list(*list);
 				continue;
 			}
-			break;
-		}
-
-		for (t_list = *list; t_list != NULL; t_list = t_list->prev)
-		{
-			*list = t_list;
-			print_list(*list);
 		}
 	}
 }
-
 /**
  * swap_nodes - function that swaps the nodes of a doubly linked list
  * @nodeL: the left node
@@ -47,10 +40,8 @@ void swap_nodes(listint_t *nodeL, listint_t *nodeR)
 	nodeR->next = nodeL;
 	nodeR->prev = nodeL->prev;
 	nodeL->prev = nodeR;
-
 	if (nodeR->prev != NULL)
 		nodeR->prev->next = nodeR;
-
 	if (nodeL->next != NULL)
 		nodeL->next->prev = nodeL;
 }
