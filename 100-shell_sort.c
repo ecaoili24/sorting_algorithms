d0d6d1e38f10dbd9e49e92799778a0d7f550bed0
kevapostol <apostolkevin@gmail.com>
2020-02-05 20:33:27 +0000

    Shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * shell_sort - selection sort
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: None
+ */
+void shell_sort(int *array, size_t size)
+{
+	int h = 1;
+	int i, n;
+
+    while (h < (int) size / 3)
+	{
+		h = 3 * h + 1;
+    }
+
+	while (h > 0)
+	{
+		for (i = h; i < (int) size; i += 1)
+		{
+			for (n = i; n > 0 && array[n] < array[n-h]; n -= h) 
+			{
+				int number = array[n];
+				array[n] = array[n-h];
+				array[n-h] = number;	
+			}	
+		}	
+		h = (h - 1) / 3;
+		print_array(array, size);
+    }
+}
