e5f38d981f1117e63eb0f36c017e7e2bf0a607af
ecaoili24 <erika.caoili@gmail.com>
2020-02-05 20:44:55 +0000

    fixed betty issues of spacing and trailing whitespace

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,23 +12,24 @@ void shell_sort(int *array, size_t size)
 	int h = 1;
 	int i, n;
 
-    while (h < (int) size / 3)
+	while (h < (int) size / 3)
 	{
 		h = 3 * h + 1;
-    }
+	}
 
 	while (h > 0)
 	{
 		for (i = h; i < (int) size; i += 1)
 		{
-			for (n = i; n > 0 && array[n] < array[n-h]; n -= h) 
+			for (n = i; n > 0 && array[n] < array[n - h]; n -= h)
 			{
 				int number = array[n];
-				array[n] = array[n-h];
-				array[n-h] = number;	
-			}	
-		}	
+
+				array[n] = array[n - h];
+				array[n - h] = number;
+			}
+		}
 		h = (h - 1) / 3;
 		print_array(array, size);
-    }
+	}
 }
