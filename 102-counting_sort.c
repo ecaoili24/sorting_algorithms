e3e24c22a0bf50c9a525367cd35ee578681b88e4
kevapostol <apostolkevin@gmail.com>
2020-02-06 03:04:22 +0000

    Modify the code

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,20 +11,17 @@ void counting_sort(int *array, size_t size)
 	size_t i, j, k, m, n, max = array[0];
 	int *count_arr, *output;
 
-	if (array == NULL)
+	if (array == NULL || size < 2)
 		return;
-	if (size < 2)
-		return;
+
 	for (i = 1; i < size; i++)
 	{
 		if (array[i] > (int) max)
 			max = array[i];
 	}
-	count_arr = malloc(sizeof(int) * (max + 1));
+	count_arr = calloc(sizeof(int), max + 1);
 	if (count_arr == NULL)
 		return;
-	for (i = 0; i <= max; i++)
-		count_arr[i] = 0;
 
 	for (j = 0; j < size; j++)
 		count_arr[array[j]] += 1;
@@ -41,8 +38,8 @@ void counting_sort(int *array, size_t size)
 	}
 	for (m = 0; m < size; m++)
 	{
+		output[count_arr[array[m]] - 1] = array[m];
 		count_arr[array[m]]--;
-		output[count_arr[array[m]]] = array[m];
 	}
 	for (n = 0; n < size; n++)
 		array[n] = output[n];
